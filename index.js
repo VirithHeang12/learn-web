265c6c2b5ac7b13099d8742706f94862763df696
Channsoriya <broya.tk123@gmail.com>
2024-09-09 15:38:56 +0700

    add route hi

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // template for express
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-    }
-);
+app.get("/hi", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(70000, () => {
-    console.log('Server is running on http://localhost:3000');
-    }
-);
+  console.log("Server is running on http://localhost:3000");
+});
